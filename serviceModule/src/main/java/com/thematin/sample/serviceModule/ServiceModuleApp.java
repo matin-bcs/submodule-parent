df72ade6c59d347612074dd21e35232f97466376
matin03 <matin03@gmail.com>
2019-06-13 16:30:09 +0900

    changed main function

--- serviceModule/src/main/java/com/thematin/sample/serviceModule/ServiceModuleApp.java
+++ serviceModule/src/main/java/com/thematin/sample/serviceModule/ServiceModuleApp.java
@@ -2,11 +2,12 @@ package com.thematin.sample.serviceModule;
 
 public class ServiceModuleApp {
 
-  /**
-   * @param args command parameter
-   */
-  public static void main(final String[] args) {
+	/**
+	 * @param args command parameter
+	 */
+	public static void main(final String[] args) {
 
-  }
+		System.out.println("changed by eclipse");
+	}
 
 }
