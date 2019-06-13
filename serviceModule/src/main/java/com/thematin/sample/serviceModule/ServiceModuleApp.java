818f6c4d56af95ea6830d96cce53f6c574d14c3e
matin03 <matin03@gmail.com>
2019-06-13 16:13:38 +0900

    new project setting

---
+++ serviceModule/src/main/java/com/thematin/sample/serviceModule/ServiceModuleApp.java
@@ -0,0 +1,12 @@
+package com.thematin.sample.serviceModule;
+
+public class ServiceModuleApp {
+
+  /**
+   * @param args command parameter
+   */
+  public static void main(final String[] args) {
+
+  }
+
+}
